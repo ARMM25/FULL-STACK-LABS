03e03ff4b022667751e601144c46006a54367a7c
Allan Martinez <allanrodrigo.martinezmalagon@georgebrown.ca>
2018-11-07 12:40:27 -0500

    last lab
    last lab

--- Lab8/exercise-1/hello-world.ts
+++ Lab8/exercise-1/hello-world.ts
@@ -1,5 +1,5 @@
-var greeter = function (name) {
-    console.log('Hello ' + name);
+{let greeter = (firstName, lastName) => {
+    console.log(`Hello ${firstName} ${lastName}`);
 }
-
-greeter("John Smith");
+greeter("John", "Smith");
+}
